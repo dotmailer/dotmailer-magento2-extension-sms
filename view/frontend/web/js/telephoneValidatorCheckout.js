2b9944f5b462f06cd82d3e82f1a72a62ff445b9e
Alastair Mucklow <amucklow@strangerpixel.com>
2023-11-17 17:35:18 +0000

    Fix indentation

--- view/frontend/web/js/telephoneValidatorCheckout.js
+++ view/frontend/web/js/telephoneValidatorCheckout.js
@@ -19,32 +19,32 @@ define([
              * @param {Object} additionalParams
              */
             validate: function (value, params, additionalParams) {
-                    var target = $('#' + additionalParams.uid),
-                        countryCodeClass = target.parent().find('.iti__selected-flag .iti__flag').attr('class'),
-                        countryCode,
-                        isValid = false,
-                        errorCode;
+                var target = $('#' + additionalParams.uid),
+                    countryCodeClass = target.parent().find('.iti__selected-flag .iti__flag').attr('class'),
+                    countryCode,
+                    isValid = false,
+                    errorCode;
 
-                    try {
-                        isValid = phoneValidate(value, countryCodeClass);
+                try {
+                    isValid = phoneValidate(value, countryCodeClass);
 
-                        if (!isValid) {
-                            countryCodeClass = countryCodeClass.split(' ')[1];
-                            countryCode = countryCodeClass.split('__')[1];
-                            errorCode = phoneErrorHandler.getErrorCode(value, countryCode);
-                            this.message = typeof errorMap[errorCode] === 'undefined' ?
-                                errorMap[0] :
-                                errorMap[errorCode];
-                        }
-                    } catch (e) {
-                        this.message = errorMap[1];
-                        isValid = false;
+                    if (!isValid) {
+                        countryCodeClass = countryCodeClass.split(' ')[1];
+                        countryCode = countryCodeClass.split('__')[1];
+                        errorCode = phoneErrorHandler.getErrorCode(value, countryCode);
+                        this.message = typeof errorMap[errorCode] === 'undefined' ?
+                            errorMap[0] :
+                            errorMap[errorCode];
                     }
+                } catch (e) {
+                    this.message = errorMap[1];
+                    isValid = false;
+                }
 
-                    // Ensure that changing the flag always updates the model
-                    ko.utils.triggerEvent(target[0], 'change');
+                // Ensure that changing the flag always updates the model
+                ko.utils.triggerEvent(target[0], 'change');
 
-                    return isValid;
+                return isValid;
             }
         };
 
