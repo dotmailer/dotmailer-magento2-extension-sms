9738b5113fbf27b466c46262dc04cdc87091fe81
Alastair Mucklow <Alastair.Mucklow@dotdigital.com>
2025-08-04 08:54:18 +0000

    Merged PR 67587: Stop loading the telephone input at checkout when validation is disabled
    ## What's being changed
    
    This PR prevents the IntlTelInput loading at checkout if telephone validation is disabled.
    
    ## Why it's being changed
    
    There was a regression some time back connected to a refactor for Hyvä themes. The impact is that telephone input validation would block submission / checkout even if the merchant had chosen not to use our validator.
    
    ## How to review / test this change
    
    - Enable SMS consent to be captured at checkout
    - Test the consent input with validation enabled
    - Test the consent input with validation disabled
    - See a green build for all checkout-related MFTF tests
    
    Related work items: #310380

--- Component/ConsentCollapseGroup.php
+++ Component/ConsentCollapseGroup.php
@@ -5,6 +5,7 @@ namespace Dotdigitalgroup\Sms\Component;
 use Dotdigitalgroup\Sms\Component\Consent\ConsentCheckbox;
 use Dotdigitalgroup\Sms\Component\Consent\ConsentTelephone;
 use Dotdigitalgroup\Sms\Component\Consent\ConsentText;
+use Dotdigitalgroup\Sms\Model\Config\Configuration;
 use Dotdigitalgroup\Sms\ViewModel\TelephoneInputConfig;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Store\Model\StoreManagerInterface;
@@ -27,9 +28,9 @@ class ConsentCollapseGroup
     private $consentText;
 
     /**
-     * @var StoreManagerInterface $storeManager
+     * @var Configuration
      */
-    private $storeManager;
+    private $moduleConfig;
 
     /**
      * @var TelephoneInputConfig
@@ -40,38 +41,36 @@ class ConsentCollapseGroup
      * @param ConsentCheckbox $consentCheckbox
      * @param ConsentTelephone $consentTelephone
      * @param ConsentText $consentText
-     * @param StoreManagerInterface $storeManager
+     * @param Configuration $moduleConfig
      * @param TelephoneInputConfig $telephoneInputConfig
      */
     public function __construct(
         ConsentCheckbox $consentCheckbox,
         ConsentTelephone $consentTelephone,
         ConsentText $consentText,
-        StoreManagerInterface $storeManager,
+        Configuration $moduleConfig,
         TelephoneInputConfig $telephoneInputConfig
     ) {
         $this->consentCheckbox = $consentCheckbox;
         $this->consentTelephone = $consentTelephone;
         $this->consentText = $consentText;
-        $this->storeManager = $storeManager;
+        $this->moduleConfig = $moduleConfig;
         $this->telephoneInputConfig = $telephoneInputConfig;
     }
 
     /**
      * Render.
      *
+     * @param string|int $storeId
+     *
      * @return array
-     * @throws NoSuchEntityException
      */
-    public function render()
+    public function render($storeId)
     {
-        $storeId = $this->storeManager->getStore()->getId();
-        $config = $this->telephoneInputConfig->getConfig();
-        return [
+        $group = [
             'component' => 'Dotdigitalgroup_Sms/js/view/consentCheckoutForm',
             'config' => [
-                'template' => 'Dotdigitalgroup_Sms/consent-checkout-form',
-                'intlTelInputConfig' => $config,
+                'template' => 'Dotdigitalgroup_Sms/consent-checkout-form'
             ],
             'provider' => 'checkoutProvider',
             'children' => [
@@ -92,5 +91,9 @@ class ConsentCollapseGroup
                 ]
             ]
         ];
+        if ($this->moduleConfig->isPhoneNumberValidationEnabled($storeId)) {
+            $group['config']['intlTelInputConfig'] = $this->telephoneInputConfig->getConfig();
+        }
+        return $group;
     }
 }
